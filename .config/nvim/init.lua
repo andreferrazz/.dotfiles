932aebba5175ddd5154f9ecd51d72df2132aa199
andreferrazz <oandreferraz@gmail.com>
2024-06-04 01:43:18 -0300

    Add some neovim config.

---
+++ .config/nvim/init.lua
@@ -0,0 +1,6 @@
+vim.opt.relativenumber = true
+vim.opt.scrolloff = 8
+vim.opt.tabstop = 4        -- Number of spaces that a tab in the file counts for
+vim.opt.shiftwidth = 4     -- Number of spaces to use for each step of indent
+vim.opt.expandtab = true   -- Use spaces instead of tabs
+
